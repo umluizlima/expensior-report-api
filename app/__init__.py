79a967b59f3a6418cb2aab95e1da3354c7d5bc99
umluizlima <umluizlima@gmail.com>
2019-03-12 23:43:39 -0300

    Add API endpoint

--- app/__init__.py
+++ app/__init__.py
@@ -1,12 +1,28 @@
 import os
-from flask import Flask
+from flask import Flask, request, jsonify
+from pymongo import MongoClient
 
 
 def create_app():
     app = Flask(__name__)
 
+    client = MongoClient(
+        os.environ.get('DATABASE_URL')
+        or 'mongodb://localhost:27017/'
+    )
+    db = client.expensior_database
+    entries = db.entries_collection
+
     @app.route("/")
     def home():
         return "Welcome home."
+
+
+    @app.route("/api/entries", methods=['POST'])
+    def create_entry():
+        data = request.get_json() or {}
+        return (str(entries.insert_one(data).inserted_id), 201) \
+            if data \
+            else ('Bad Request', 400)
 
     return app
